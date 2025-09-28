982ab075fe6e1301fec0e0b34b6460adad4afdf1
try-kode <leangtrysam656@gmail.com>
2025-09-28 11:15:42 +0700

    add image

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
 }
